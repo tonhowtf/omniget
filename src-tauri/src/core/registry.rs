df62c3aaa1edac013edfa49fdadc46c4cb10cd08
Tonho <tonhowtf@gmail.com>
2026-02-15 17:24:06 -0300

    feat: update PlatformRegistry to use Arc instead of Box for platform downloaders

--- src-tauri/src/core/registry.rs
+++ src-tauri/src/core/registry.rs
@@ -1,7 +1,9 @@
+use std::sync::Arc;
+
 use crate::platforms::traits::PlatformDownloader;
 
 pub struct PlatformRegistry {
-    platforms: Vec<Box<dyn PlatformDownloader>>,
+    platforms: Vec<Arc<dyn PlatformDownloader>>,
 }
 
 impl PlatformRegistry {
@@ -11,15 +13,15 @@ impl PlatformRegistry {
         }
     }
 
-    pub fn register(&mut self, platform: Box<dyn PlatformDownloader>) {
+    pub fn register(&mut self, platform: Arc<dyn PlatformDownloader>) {
         self.platforms.push(platform);
     }
 
-    pub fn find_platform(&self, url: &str) -> Option<&dyn PlatformDownloader> {
+    pub fn find_platform(&self, url: &str) -> Option<Arc<dyn PlatformDownloader>> {
         self.platforms
             .iter()
             .find(|p| p.can_handle(url))
-            .map(|p| p.as_ref())
+            .cloned()
     }
 }
 
