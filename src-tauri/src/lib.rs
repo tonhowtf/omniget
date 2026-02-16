dc5dc962c09f649598942111bd7772bd1ed4c722
Tonho <tonhowtf@gmail.com>
2026-02-16 11:00:57 -0300

    feat: add updater and process plugins for enhanced functionality

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -81,6 +81,8 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![
             commands::auth::hotmart_login,
             commands::auth::hotmart_check_session,
