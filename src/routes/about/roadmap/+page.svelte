f224083239cd7e67ca946959d80d10d41cae6a6f
Tonho <tonhowtf@gmail.com>
2026-02-16 10:07:28 -0300

    feat: update checklist items to use div elements for consistency

--- src/routes/about/roadmap/+page.svelte
+++ src/routes/about/roadmap/+page.svelte
@@ -7,28 +7,28 @@
 <section class="card">
   <h5 class="card-title">{$t('about.roadmap_platforms_title')}</h5>
   <div class="checklist">
-    <label class="check-item done"><span class="checkbox">✓</span> YouTube</label>
-    <label class="check-item done"><span class="checkbox">✓</span> Instagram</label>
-    <label class="check-item done"><span class="checkbox">✓</span> TikTok</label>
-    <label class="check-item done"><span class="checkbox">✓</span> Twitter / X</label>
-    <label class="check-item done"><span class="checkbox">✓</span> Reddit</label>
-    <label class="check-item done"><span class="checkbox">✓</span> Twitch</label>
-    <label class="check-item done"><span class="checkbox">✓</span> Pinterest</label>
-    <label class="check-item done"><span class="checkbox">✓</span> Bluesky</label>
-    <label class="check-item done"><span class="checkbox">✓</span> Hotmart</label>
-    <label class="check-item"><span class="checkbox"></span> Hubla</label>
-    <label class="check-item"><span class="checkbox"></span> Kiwify</label>
-    <label class="check-item"><span class="checkbox"></span> Udemy</label>
+    <div class="check-item done"><span class="checkbox">✓</span> YouTube</div>
+    <div class="check-item done"><span class="checkbox">✓</span> Instagram</div>
+    <div class="check-item done"><span class="checkbox">✓</span> TikTok</div>
+    <div class="check-item done"><span class="checkbox">✓</span> Twitter / X</div>
+    <div class="check-item done"><span class="checkbox">✓</span> Reddit</div>
+    <div class="check-item done"><span class="checkbox">✓</span> Twitch</div>
+    <div class="check-item done"><span class="checkbox">✓</span> Pinterest</div>
+    <div class="check-item done"><span class="checkbox">✓</span> Bluesky</div>
+    <div class="check-item done"><span class="checkbox">✓</span> Hotmart</div>
+    <div class="check-item"><span class="checkbox"></span> Hubla</div>
+    <div class="check-item"><span class="checkbox"></span> Kiwify</div>
+    <div class="check-item"><span class="checkbox"></span> Udemy</div>
   </div>
 </section>
 
 <section class="card">
   <h5 class="card-title">{$t('about.roadmap_features_title')}</h5>
   <div class="checklist">
-    <label class="check-item done"><span class="checkbox">✓</span> {$t('about.roadmap_feature_themes')}</label>
-    <label class="check-item done"><span class="checkbox">✓</span> {$t('about.roadmap_feature_i18n')}</label>
-    <label class="check-item done"><span class="checkbox">✓</span> {$t('about.roadmap_feature_hotmart_login')}</label>
-    <label class="check-item"><span class="checkbox"></span> {$t('about.roadmap_feature_instagram_login')}</label>
+    <div class="check-item done"><span class="checkbox">✓</span> {$t('about.roadmap_feature_themes')}</div>
+    <div class="check-item done"><span class="checkbox">✓</span> {$t('about.roadmap_feature_i18n')}</div>
+    <div class="check-item done"><span class="checkbox">✓</span> {$t('about.roadmap_feature_hotmart_login')}</div>
+    <div class="check-item"><span class="checkbox"></span> {$t('about.roadmap_feature_instagram_login')}</div>
   </div>
 </section>
 
