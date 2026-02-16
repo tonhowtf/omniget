afa768c19d618ccb6929a587dacdce26c08e6b81
Tonho <tonhowtf@gmail.com>
2026-02-15 21:13:57 -0300

    refactor: simplify translation loading by removing browser language detection

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,8 +3,6 @@ import { loadTranslations, defaultLocale } from "$lib/i18n";
 export const ssr = false;
 
 export const load = async ({ url }) => {
-  const browserLang = navigator.language || defaultLocale;
-  const locale = browserLang.startsWith("en") ? "en" : "pt";
-  await loadTranslations(locale, url.pathname);
+  await loadTranslations(defaultLocale, url.pathname);
   return {};
 };
