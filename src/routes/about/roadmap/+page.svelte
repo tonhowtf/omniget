42632064fed3268a9db63b9528a028cf8c1938d0
Tonho <tonhowtf@gmail.com>
2026-02-16 10:05:38 -0300

    feat: add roadmap page with description and styling

---
+++ src/routes/about/roadmap/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { t } from "$lib/i18n";
+</script>
+
+<div class="roadmap">
+  <p class="roadmap-description">{$t('about.roadmap_description')}</p>
+</div>
+
+<style>
+  .roadmap {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: calc(var(--padding) * 2) 0;
+  }
+
+  .roadmap-description {
+    font-size: 14.5px;
+    font-weight: 500;
+    color: var(--gray);
+    text-align: center;
+  }
+</style>
