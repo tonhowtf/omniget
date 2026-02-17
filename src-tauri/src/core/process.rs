7c46c08cea63f9a9a7c80d60848dc0d170390ca0
Tonho <tonhowtf@gmail.com>
2026-02-17 14:25:13 -0300

    feat: add functions to create commands that hide console window on Windows

---
+++ src-tauri/src/core/process.rs
@@ -0,0 +1,18 @@
+/// Creates a tokio Command that hides the console window on Windows.
+pub fn command<S: AsRef<std::ffi::OsStr>>(program: S) -> tokio::process::Command {
+    let mut cmd = tokio::process::Command::new(program);
+    #[cfg(target_os = "windows")]
+    cmd.creation_flags(0x08000000);
+    cmd
+}
+
+/// Creates a std Command that hides the console window on Windows.
+pub fn std_command<S: AsRef<std::ffi::OsStr>>(program: S) -> std::process::Command {
+    let mut cmd = std::process::Command::new(program);
+    #[cfg(target_os = "windows")]
+    {
+        use std::os::windows::process::CommandExt;
+        cmd.creation_flags(0x08000000);
+    }
+    cmd
+}
