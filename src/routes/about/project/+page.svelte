2c537427574baacc25c87363cc51b68523aa10ab
Tonho <tonhowtf@gmail.com>
2026-02-18 12:43:36 -0300

    feat: update version to 0.2.9 in Cargo files and application display

--- src/routes/about/project/+page.svelte
+++ src/routes/about/project/+page.svelte
@@ -3,7 +3,7 @@
   import { t } from "$lib/i18n";
 
   const GITHUB_URL = "https://github.com/tonhowtf/omniget";
-  const APP_VERSION = "0.2.8";
+  const APP_VERSION = "0.2.9";
 
   async function openGitHub() {
     await open(GITHUB_URL);
