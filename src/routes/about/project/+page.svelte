164d4695afa3ec96abf763728ee85fb3343d5e05
Tonho <tonhowtf@gmail.com>
2026-02-19 10:39:25 -0300

    feat: update app version to 0.2.12 in +page.svelte

--- src/routes/about/project/+page.svelte
+++ src/routes/about/project/+page.svelte
@@ -3,7 +3,7 @@
   import { t } from "$lib/i18n";
 
   const GITHUB_URL = "https://github.com/tonhowtf/omniget";
-  const APP_VERSION = "0.2.11";
+  const APP_VERSION = "0.2.12";
 
   async function openGitHub() {
     await open(GITHUB_URL);
