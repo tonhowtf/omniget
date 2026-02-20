2d23b659551dc800ef49dd0cac55fe4361d9d016
Tonho <tonhowtf@gmail.com>
2026-02-19 22:18:46 -0300

    feat: add global shortcut and clipboard manager plugins to the application

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -127,6 +127,8 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_clipboard_manager::init())
         .setup(|app| {
             tray::setup(app.handle())?;
             Ok(())
