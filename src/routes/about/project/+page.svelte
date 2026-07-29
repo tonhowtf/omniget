52ef84e09749f32f1c993beaf1b6dbda5b5a4443
tonhowtf <tonhowtf@gmail.com>
2026-07-29 20:34:38 -0300

    chore(release): 0.8.1
    Bump nos cinco arquivos de versao mais a entrada de release no metainfo.xml,
    que e de onde sai o changelog publico e o que os software centers do Linux
    mostram.

--- src/routes/about/project/+page.svelte
+++ src/routes/about/project/+page.svelte
@@ -3,7 +3,7 @@
   import { t } from "$lib/i18n";
 
   const GITHUB_URL = "https://github.com/tonhowtf/omniget";
-  const APP_VERSION = "0.8.0";
+  const APP_VERSION = "0.8.1";
 
   async function openGitHub() {
     await open(GITHUB_URL);
